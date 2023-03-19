09e2407b13a1688397d0bd332a99de307c70221b
developer-shourav <mrinmoyraj823@gmail.com>
2023-03-19 07:29:33 +0600

    dynamic data adding complete

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const loadData = () => {
 const displayData = cardsData => {
   const cardsContainer = document.getElementById('cards');
   cardsData.forEach( item => {
+    console.log(item);
     const card = document.createElement('div');
     card.classList.add('card', 'm-2');
     card.innerHTML = `
@@ -18,14 +19,14 @@ const displayData = cardsData => {
   <div class="product-img-container">
     <img
       class="product-img"
-      src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+      src="${item?.image}"
       alt=""
     />
   </div>
-  <h3>Widget 3000</h3>
-  <p>The Widget 3000 is the latest and greatest in widget</p>
+  <h3>${item.name}</h3>
+  <p>${item.description.slice(0, 60)}..</p>
   <div class="priceAndButtons">
-    <h2 class="text-primary">$580</h2>
+    <h2 class="text-primary">$${item.price}</h2>
     <button class="btn btn-primary">Buy Now</button>
   </div>
     `;
